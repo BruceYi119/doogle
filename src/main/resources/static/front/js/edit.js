8ad5264f6efaa14c4f3f2e6390b9720d1e2d2900
ysj <pirates119@gmail.com>
2021-02-19 14:32:25 +0900

    modify edit.js

--- src/main/resources/static/front/js/edit.js
+++ src/main/resources/static/front/js/edit.js
@@ -8,8 +8,8 @@ const editOption = {
 	minHeight: 100,
 	maxHeight: 500,
 	focus: true,									// set focus to editable area after initializing summe
-	fontNames: ['돋움', '궁서', '맑은 고딕', '함초롬바탕', 'Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Merriweather'],
-	fontNamesIgnoreCheck: ['Merriweather']
+	fontNames: ['돋움', '궁서', '맑은 고딕', '함초롬바탕', 'Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Merriweather']
+	//fontNamesIgnoreCheck: ['Merriweather']
 };
 
 const initEdit = (option = editOption) => {
