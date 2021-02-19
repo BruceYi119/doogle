46ae4415d501548af2e8ac3184c08343892e0f17
ysj <pirates119@gmail.com>
2021-02-19 14:31:00 +0900

    modify edit files

--- src/main/resources/static/front/js/form.js
+++ src/main/resources/static/front/js/form.js
@@ -1,9 +1,1 @@
-initEdit({
-	lang: 'ko-KR', // default: 'en-US'
-	placeholder: 'Hello Doogle Shopping!',
-	tabsize: 2,
-	height: 300,
-	minHeight: null,
-	maxHeight: 500,
-	focus: true                  // set focus to editable area after initializing summe
-});
+initEdit();
