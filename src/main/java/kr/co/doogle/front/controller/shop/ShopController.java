09d1ebcb69dd65c3408209a3e5c511b8e4e46382
ysj <pirates119@gmail.com>
2021-02-21 17:51:42 +0900

    add controller pakage shop

--- src/main/java/kr/co/doogle/front/controller/shop/ShopController.java
+++ src/main/java/kr/co/doogle/front/controller/shop/ShopController.java
@@ -1,4 +1,4 @@
-package kr.co.doogle.front.controller;
+package kr.co.doogle.front.controller.shop;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
